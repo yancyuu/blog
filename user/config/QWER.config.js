6b332a85e9a28532a776402389d48930fad3aad6
yancyuu <56645657+yancyuu@users.noreply.github.com>
2022-11-28 21:27:33 +0800

    修改默认文章语言

--- user/config/QWER.config.js
+++ user/config/QWER.config.js
@@ -2,7 +2,7 @@ export const UserConfig = {
   DefaultDesktopShowTagFilter: false,
   DefaultCoverStyle: 'IN',
   DefaultTagName: 'tags',
-  DefaultPostLanguage: 'English',
+  DefaultPostLanguage: 'Chinese',
   PostLanguageTagName: 'Language',
   SeriesTagName: 'Series',
   YearTagName: 'Year',
